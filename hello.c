3920edc691e24175402503d6db63ce6e2d88b93a
srevesz2 <sophia.revesz@huskers.unl.edu>
2018-08-23 08:19:31 -0500

    Initial Commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Artiom Dolghi and Sophia Revesz
+ * Date: 2018/21/08
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Artiom Dolghi and Sophia Revesz\n");
+
+  return 0;
+}
